4db3e1f4ed0dc5ede4d6011c75b59d39adf92c51
一维空间 <34644713+fbdxl@users.noreply.github.com>
2023-01-29 14:39:12 +0800

    commit before SMO control performance test，滑模观测器控制性能指标测试之前上传存档
    commit before SMO control performance test，滑模观测器控制性能指标测试之前上传存档

--- SMO_conventional/Calc_We_ThetaErr_Estimate_Harmonics.m
+++ SMO_conventional/Calc_We_ThetaErr_Estimate_Harmonics.m
@@ -1,4 +1,4 @@
-fosc=1e6;N=1e6;
+fosc=1e4;N=1e4;
 Tosc=N/fosc;
 
 WeEstRaw=out.WeEst.signals(1).values(end-N+1:end,1);
@@ -18,10 +18,7 @@ phase2=angle(y1(1:N/2+1,:))*180/pi;
 
 
 fseq=fosc*(0:N/2)/N;
-phaseErr=mod(phase2-phase1,360);
-phaseErr=phaseErr - floor(phaseErr/180)*360;
-EndFreq=10e3;
-EndIndex=find(fseq>10e3,1);
+EndIndex=500;
 
 figure(1);
 subplot(2,1,1);
